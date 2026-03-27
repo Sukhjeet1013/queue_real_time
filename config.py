abc2b9ca99d2e25d10442b1d000443b752380c46
Sukhjeet Singh Kaunsal <156096147+Sukhjeet1013@users.noreply.github.com>
2026-03-28 00:15:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    db_url = os.getenv("DATABASE_URL")
+    db_url = os.getenv("https://dashboard.render.com/d/dpg-d73cv6khg0os73ba4kn0-a")
 
     # Fix Render PostgreSQL URL issue
     if db_url and db_url.startswith("postgres://"):
