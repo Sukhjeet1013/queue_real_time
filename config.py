4985a50d73daea337d8173ce89cc91d393a8d800
unknown <kaunsalsukhjeetsingh@gmail.com>
2026-03-28 11:08:32 +0530

    Fixed DATABASE_URL config

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
 class Config:
-    db_url = os.getenv("https://dashboard.render.com/d/dpg-d73cv6khg0os73ba4kn0-a")
+    db_url = os.getenv("DATABASE_URL")
 
-    # Fix Render PostgreSQL URL issue
+    # Fix Railway / Render postgres URL issue
     if db_url and db_url.startswith("postgres://"):
         db_url = db_url.replace("postgres://", "postgresql://", 1)
 
-    # Fallback for local development
+    # Optional fallback (only for local testing)
     if not db_url:
-        db_url = "postgresql://postgres:password@localhost:5432/smartqueue"
+        db_url = "sqlite:///local.db"
 
     SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
